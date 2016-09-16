a6352f3cc017e47d0cb7c1192a588d50fd792a49
Jean-Christophe Gay <jean-christophe.gay@dauphine.fr>
2016-09-16 07:47:05 +0200

    Creation d'un ebbok avec 3 pages, une couv et 2 images

--- tmp.py
+++ tmp.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 from ebooklib import epub
 
 book = epub.EpubBook()
@@ -11,20 +13,31 @@ book.add_author('Tarrke')
 
 book.set_cover("image.jpg", open('cover.jpg', 'rb').read())
 
-# create chapter
-c1 = epub.EpubHtml(title='Intro-spection', file_name='chap_01.xhmtl', lang='en')
-c1.content=u'<h1>Introduction</h1><p>Salut le monde.</p>'
 
-c2 = epub.EpubHtml(title='About this book', file_name='about.xhtml', lang='en')
-c2.content='<h1>About this book</h1><p>Helou, this is my book! There are many books, but this one is mine.</p>'
+# create page 1
+c1 = epub.EpubHtml(title='Page #1', file_name='page_1.xhtml', lang='en')
+c1.content=u'<div><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="100%" height="100%" viewBox="0 0 590 750" preserveAspectRatio="none"><image width="590" height="750" xlink:href="1_0.png"/></svg></div>'
+
+c2 = epub.EpubHtml(title='Page #2', file_name='page_2.xhtml', lang='en')
+c2.content=u'<div><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="100%" height="100%" viewBox="0 0 590 750" preserveAspectRatio="none"><image width="590" height="750" xlink:href="2_0.png"/></svg></div>'
 
 book.add_item(c1)
 book.add_item(c2)
 
-book.toc = (epub.Link('chap_01.xhtml', 'Introduction', 'intro'), 
-			(epub.Section('Simple book'), 
-			(c1, c2))
-			)
+im1 = open('1_0.png', 'rb').read()
+im2 = open('2_0.png', 'rb').read()
+
+img1 = epub.EpubItem(uid="1_0.png", file_name="1_0.png", media_type="image/png", content=im1)
+img2 = epub.EpubItem(uid="2_0.png", file_name="2_0.png", media_type="image/png", content=im2)
+
+
+book.add_item(img1)
+book.add_item(img2)
+
+#book.toc = (epub.Link('chap_01.xhtml', 'Introduction', 'intro'), 
+#			(epub.Section('Simple book'), 
+#			(c1, c2))
+#			)
 
 book.add_item(epub.EpubNcx())
 book.add_item(epub.EpubNav())
@@ -56,6 +69,6 @@ nav[epub|type~='toc'] > ol > li > ol > li {
 nav_css = epub.EpubItem(uid="style_nav", file_name="style/nav.css", media_type="text/cass", content=style)
 book.add_item(nav_css)
 
-book.spine = ['cover', 'nav', c1, c2]
+book.spine = ['cover', c1, c2]
 
 epub.write_epub('test.epub', book, {})
